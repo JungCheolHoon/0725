47e4fa5e5bfdf6faa0cf95e03f9f805ecc1aa488
JungCheolHoon <cjswovkdnj12@naver.com>
2022-07-25 17:53:41 +0900

    Create Project

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+public class HelloWorld {
+	public static void main(String[] args) {
+//		System.out.println("***************************************");
+//		System.out.println("Hello, World");		
+//		System.out.println("***************************************");
+		
+		System.out.print("당신의 이름은 ? : ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		
+		System.out.println("귀하의 이름은 " + name + "이군요.");
+	}
+}
